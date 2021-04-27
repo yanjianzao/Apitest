30740ae78d1587037aae8736eac5039cb45d5107
jianzao <732678737@qq.com>
2021-04-27 22:52:09 +0800

    'init'

---
+++ testcese/testrequest.py
@@ -0,0 +1,11 @@
+import requests
+import logging
+import pytest
+import json
+class TestRequest(object):
+    logging.basicConfig(level=logging.INFO)
+    def test_get(self):
+        r = requests.get("https://testerhome.com/api/v3/topics.json?limit=2")
+        logging.info(r)
+        logging.info(r.text)
+        logging.info(json.dumps(r.json(),indent=2))
