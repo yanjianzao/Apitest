544fd3d8cda97054dfb7381bfd316b175d804efb
jianzao <732678737@qq.com>
2021-04-27 23:34:10 +0800

    init

--- testcese/testrequest.py
+++ testcese/testrequest.py
@@ -2,10 +2,12 @@ import requests
 import logging
 import pytest
 import json
+logging.basicConfig(level=logging.INFO)
 class TestRequest(object):
-    logging.basicConfig(level=logging.INFO)
     def test_get(self):
         r = requests.get("https://testerhome.com/api/v3/topics.json?limit=2")
-        logging.info(r)
-        logging.info(r.text)
-        logging.info(json.dumps(r.json(),indent=2))
+        print(json.dumps(r.json(),indent=2))
+
+
+
+
