5cd83ac06e3a61390ff80d73a8f9c5de79ed5adf
yanjianzao2020 <yanjianzao2020@163.com>
2021-04-28 10:41:47 +0800

    test

--- testcese/testrequest.py
+++ testcese/testrequest.py
@@ -1,18 +1,29 @@
+#coding: utf-8
 import requests
 import logging
 import pytest
 import json
-logging.basicConfig(level=logging.INFO)
+handleapp = {
+    "Host":"gatewaytest.jinlingkeji.cn",
+    "Connection": "keep-alive",
+    "Content type":"application/x-www-form-urlencoded;charset=UTF-8",
+    "Accept": "application/json, text/plain, */*",
+    "Accept-Language": "zh-cn",
+    "User-Agent":"%E7%BD%91%E4%B8%8A%E8%80%81%E5%B9%B4%E5%A4%A7%E5%AD%A6/2.8 CFNetwork/1128.0.1 Darwin/19.6.0",
+    "app-version":"2.3.0",
+    "source-type": "IOS",
+    "Authorization":"eyJhbGciOiJIUzUxMiJ9.eyJ1aWQiOjM0OTE5OCwibG9naW5UaW1lIjoiMTYxNjEzODMwNDQzOSIsInBob25lIjoiMTUzNjE1NjA4ODgiLCJ0eXBlIjoiMCIsImV4cCI6MTYyMzkxNDMwNH0.f6vRDL5yYMQ6M7WepfjwqGEqejht1LYLhfDfv2ECQGrLuRWBL4jUYTNgDeSTSdCS6YzZXqFqxmMGiSP9RAj_Hg"
+}
 class TestRequest(object):
     url = "https://testerhome.com/api/v3/topics.json?limit=2"
+    logging.basicConfig(level=logging.INFO)
     def test_get(self):
         r = requests.get(self.url,verify=False)
-        print(json.dumps(r.json(),indent=2))
+        # logging.info(json.dumps(r.json(),indent=2))
     def test_post(self):
-        r = requests.post(url=self.url,data={"a":"1","b":"2"},proxies={"https":"http://127.0.0.1:8888","http":"http://127.0.0.1:8888"},verify=False)
-        print(r.url)
-        print(r.text)
-
+        res2 = requests.post(url='https://gatewaytest.jinlingkeji.cn/v9/message/live/hot/list',
+                             json={"pageSize": "8", "pageNum": "1", "type": "1"}, headers=handleapp)
+        logging.info(json.dumps(res2.json(),indent=2))
 
 
 
